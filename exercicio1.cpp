1905541360243f8bb304931160abe3a4b797d75b
Clarice <142429120+clarisv@users.noreply.github.com>
2023-12-04 14:10:34 -0300

    exercicio1.cpp

--- exercicio1.cpp
+++ exercicio1.cpp
@@ -1,34 +1,31 @@
 #include <stdio.h>
 
 int main() {
+
     float num1, num2;
 
-    // Solicita os dois números
-    printf("Digite o primeiro número: ");
+    printf("digite o primeiro número: ");
     scanf("%f", &num1);
 
-    printf("Digite o segundo número: ");
+    printf("digite o segundo número: ");
     scanf("%f", &num2);
 
-    // Realiza as operações
     float soma = num1 + num2;
     float subtracao = num1 - num2;
     float multiplicacao = num1 * num2;
-    
-    // Verifica se o segundo número é diferente de zero antes da divisão
+
     float divisao;
     if (num2 != 0) {
         divisao = num1 / num2;
     } else {
-        printf("Não é possível dividir por zero.\n");
-        return 1; // Retorna um código de erro
+        printf("não é possível dividir por zero.\n");
+        return 1; 
     }
 
-    // Exibe os resultados
-    printf("Soma: %.2f\n", soma);
-    printf("Subtração: %.2f\n", subtracao);
-    printf("Multiplicação: %.2f\n", multiplicacao);
-    printf("Divisão: %.2f\n", divisao);
+    printf("soma: %.2f\n", soma);
+    printf("subtração: %.2f\n", subtracao);
+    printf("multiplicação: %.2f\n", multiplicacao);
+    printf("divisão: %.2f\n", divisao);
 
-    return 0; // Retorna 0 para indicar sucesso
-}
+    return 0; 
+
